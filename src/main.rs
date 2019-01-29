d41ad71db6a9ad911e6de9d55af5062aea905e2f
David Orchard <if_coding@fastmail.com>
2019-01-29 11:39:55 -0800

    Add TODO, calendar output

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ use std::io::BufRead;
 
 fn main() -> Result<(), Error> {
     let team_name = "Real Portland".to_string().to_uppercase();
-    // todo: trim results
+
     let r = Regex::new(r"^[A-Z]{3} [A-Z]{3} [0-9 ]{2} +[0-9 ]{2}:[0-9]{2} [AP]M  (.*) vs (.*)$").unwrap();
+    let mut calendar = Calendar::new();
+
     for line in std::io::stdin().lock().lines() {
         let line = line.unwrap();
         let groups = r.captures(&line);
@@ -17,13 +19,26 @@ fn main() -> Result<(), Error> {
             let home = groups.get(1).unwrap().as_str().trim();
             let away = groups.get(2).unwrap().as_str().trim();
             if home == team_name || away == team_name {
+                // TODO: test fall schedule for new year's rollover edge case
                 let line = line.split_at(22).0.to_string() + "2019";
-                println!("{}", line);
+                //println!("{}", line);
                 let date = US::Pacific.datetime_from_str(&line, "%a %b %e %I:%M %p %Y").with_context(|e| {format!("Error parsing datetime string: {}", e)})?;
+
+                let event = Event::new()
+                    .summary(&(home.to_string() + " (home) vs." + away))
+                    .description("Home team brings ball & all colors")
+                    // TODO: location?
+                    // TODO: set busy
+                    .starts(date)
+                    .ends(date + Duration::minutes(44+2))
+                    .done();
+
+                calendar.push(event);
             }
         }
     }
 
+    calendar.print().unwrap();
     Ok(())
 }
 
